2fa987fd69dfef14a05868d3a133dd69c03b70ae
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-05-23 18:00:26 -0300

    Atualiza componente de header para utilizar reactstrap

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import styled from 'styled-components'
+
+import { Container, Row, Collapse, Navbar, NavbarToggler, NavbarBrand } from 'reactstrap';
+
+import { Menu } from './'
+
+const StyledHeader = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <StyledHeader isOpen={isOpen}>
+      <Container>
+        <Row>
+        <Navbar className="col" light expand="md">
+          <NavbarBrand href="/">Vota Cidade</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Menu />
+          </Collapse>
+        </Navbar>
+        </Row>
+      </Container>
+    </StyledHeader>
+  )
+}
