1e9852c76d55c85b4ec753bdec82ba0f477ad272
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-05-23 18:02:03 -0300

    Atualiza chamada do componente de layout das paginas existentes

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -2,7 +2,7 @@
 import React from "react"
 import { PageProps, Link } from "gatsby"
 
-import Layout from "../components/layout"
+import { Layout } from "../components/Layout"
 import SEO from "../components/seo"
 
 const SecondPage = (props: PageProps) => (
