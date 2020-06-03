87bb3a292687a24d3a0df57f1acf7f374b7677cb
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-06-02 23:51:18 -0300

    Utiliza novo componente de cta de email

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,19 @@
 import React from "react"
-import { Container, Row, Col, Form, FormGroup, Input } from 'reactstrap'
 
 import { Layout } from "../components/Layout"
 import { Link } from "../components/Link"
+import { CTAEmail } from "../components/CTAEmail"
 import Image from "../components/image"
-import { Button } from '../components/Button'
 import SEO from "../components/seo"
-
-const CTAEmail = ({ title }) => {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>{title}</h1>
-        </Col>
-      </Row>
-      <Form action="">
-        <Row from>
-          <Col lg="4">
-            <FormGroup>
-              <Input type="mail" name="email" placeholder="Digite seu e-mail" />
-            </FormGroup>
-          </Col>
-          <Col lg="4">
-            <FormGroup>
-              <Input type="cidade" name="cidade" placeholder="Digite sua cidade" />
-            </FormGroup>
-          </Col>
-          <Col lg="4">
-            <Button block type="submit">Enviar</Button>
-          </Col>
-        </Row>
-      </Form>
-    </Container>
-  )
-}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <header style={{ backgroundColor: '#BABABA' }} className="pt-5 pb-5">
+      <CTAEmail
+        title="Chamada para se inscrever e ser avisado do lançamento"
+      />
+    </header>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
