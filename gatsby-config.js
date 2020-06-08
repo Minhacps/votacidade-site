901ffe9487cc273532f5bde3c151f4403e45619f
André Luís Bordignon <albordignon@gmail.com>
2020-06-08 12:03:52 -0300

    Iniciando o trabalho com a página do projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
          link:'/'
       },
       {
-         name:'page2',
-         link:'/page-2'
+         name:'Projeto',
+         link:'/page-project'
       }
     ]
   },
---
+++ src/components/PageProject/ProjectDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Container, Row, Col } from 'reactstrap'
+import Title from '../Title';
+
+export const ProjectDetails = () => {
+  return (
+    <Container>
+      <Row>
+
+      </Row>
+    </Container>
+  )
+}
