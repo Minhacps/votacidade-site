641a40086ea1eb3261c790ee5ffdfeabc64172db
joaodias <joaovitordias92040@gmail.com>
2020-06-08 14:43:03 -0300

    Continuando o trabalho da página de projeto

--- src/components/PageProject/ProjectDetails.js
+++ src/components/PageProject/ProjectDetails.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import styled from 'styled-components';
 
-import { Container, Row, Col } from 'reactstrap'
-import Title from '../Title';
+const StyledTitle = styled.h3`
+  color: #662D91;
+  font-weight: ${({ weight }) => weight || 700};
+  ${({ size }) => size && `font-size: ${size}`};
+`
 
-export const ProjectDetails = () => {
+const P = styled.p`
+  font-family: Roboto;
+  font-size: 18px;
+  margin-top: 24px; 
+`;
+
+const Coluna = styled.div`
+  margin-top: 120px;
+  margin-bottom: 120px;
+`
+
+
+const ProjectDetails = () => {
   return (
     <Container>
       <Row>
+        <Coluna>
+          <h1 style={{fontSize: '18px', color: '#FBB040',fontWeight: 'bold' }}>conheça o projeto</h1>
+          <StyledTitle size="40px">Chamada falando sobre a iniciativa</StyledTitle>
 
+          <P>Nesse texto podemos falar sobre o projeto, a iniciativa e as organizações que fazem parte disso. Aqui pode entrar até algum texto bem elaborado para dar introdução a contribuição em dinheiro. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
+        </Coluna>
       </Row>
     </Container>
   )
 }
+
+export default ProjectDetails;
--- src/pages/page-project.js
+++ src/pages/page-project.js
@@ -2,28 +2,29 @@ import React from "react";
 import { Container, Row, Col } from 'reactstrap';
 import { Layout } from "../components/Layout";
 import SEO from "../components/seo";
-import { CTAEmail } from "../components/CTAEmail"
 import Title from '../components/Title';
 
 import ProjectDetails from "../components/PageProject/ProjectDetails";
 
 import './PageProject.css';
 
-const PageProject = (props) => (
-  <Layout>
-    <SEO title="Page Project" />
-    <Container className='PageProjectHeader' fluid={true}>
-      <Row>
-        <Col>
-          <div>
+const PageProject = (props) => {
+  return(
+    <Layout>
+      <SEO title="Page Project" />
+      <Container className='PageProjectHeader' fluid={true}>
+        <Row>
+          <Col>
             <h1 className='title'>O Projeto</h1>
-              <h1>çladkjsfçldj</h1>
-          </div>
-        </Col>
+            
 
-      </Row>
-    </Container>
-  </Layout>
-)
+          </Col>
+        </Row>
+      </Container>
+      <ProjectDetails />
+    </Layout>
+  )
+  
+}
 
 export default PageProject
