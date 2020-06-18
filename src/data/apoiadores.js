f67b07a56bb0fffe4479da631daba98033fa0743
André Luís Bordignon <albordignon@gmail.com>
2020-06-18 12:36:10 -0300

    Incluido novos apoiadores.

--- src/data/apoiadores.js
+++ src/data/apoiadores.js
@@ -4,31 +4,19 @@ export const apoiadores = [
     logo: require('../images/logo-front-campinas.svg'),
   },
   {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
+    nome: 'IFSP Campinas',
+    logo: require('../images/logo-ifsp-campinas.png'),
   },
   {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
+    nome: 'Minha Campinas',
+    logo: require('../images/logo-minha-campinas.png'),
   },
   {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
+    nome: 'Minha Jampa',
+    logo: require('../images/logo-minha-jampa.png'),
   },
   {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
-  },
-  {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
-  },
-  {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
-  },
-  {
-    nome: 'Front Campinas',
-    logo: require('../images/logo-front-campinas.svg'),
+    nome: 'Minha Porto Alegre',
+    logo: require('../images/logo-minha-poa.png'),
   },
 ]
