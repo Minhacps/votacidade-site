e60ef07704e54ece9e3031b291b85f683116fa98
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-06-20 16:43:33 -0300

    Cria arquivo dados das cidades

---
+++ src/data/cidades.js
@@ -0,0 +1,27 @@
+export const cidades = [
+  {
+    title: 'Campinas',
+    image: 'https://www.livehere.com.br/blog/wp-content/uploads/2019/05/morar-em-campinas-990x556.jpg',
+    linkTo: 'https://www.minhacampinas.org.br/',
+  },
+  {
+    title: 'João Pessoa',
+    image: require('../images/jampa-fundo.png'),
+    linkTo: 'https://www.minhacampinas.org.br/',
+  },
+  {
+    title: "Porto Alegre",
+    image: require('../images/poa-fundo.jpg'),
+    linkTo: "https://www.minhacampinas.org.br/",
+  },
+  {
+    title: "Recife",
+    image: require('../images/poa-fundo.jpg'),
+    linkTo: "https://www.minhacampinas.org.br/",
+  },
+  {
+    title: "Campina Grande",
+    image: require('../images/poa-fundo.jpg'),
+    linkTo: "https://www.minhacampinas.org.br/",
+  },
+]
