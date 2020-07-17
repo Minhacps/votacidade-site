9d58c862280c43d02bdcfc852a1a336bf02cec02
joaodias <joaovitordias92040@gmail.com>
2020-07-17 12:23:53 -0300

    Atualizando o Calendário Eleitoral

--- src/data/calendario.js
+++ src/data/calendario.js
@@ -1,37 +1,32 @@
 export const datasDoCalendario = [
   {
-    date: '31-Ago-2020',
-    title: 'Início da convenções partidárias',
-    text: '',
-  },
-  {
-    date: '31-Ago-2020',
-    title: 'Início da registros de candidaturas',
-    text: '',
-  },
-  {
-    date: '16-Set-2020',
-    title: 'Fim convenções partidárias',
-    text: '',
-  },
-  {
-    date: '26-Set-2020',
-    title: 'Fim do registros de candidaturas',
-    text: '',
-  },
-  {
-    date: '27-Set-2020',
-    title: 'Início da propaganda eleitoral',
-    text: '',
-  },
-  {
-    date: '15-Nov-2020',
-    title: 'Primeiro turno das eleições',
-    text: '',
-  },
-  {
-    date: '29-Nov-2020',
-    title: 'Segundo turno das eleições',
-    text: '',
-  },
+  date: '31-Ago-2020',
+  title: 'Início da convenções partidárias',
+  text: '',
+},
+{
+  date: '16-Set-2020',
+  title: 'Fim convenções partidárias',
+  text: '',
+},
+{
+  date: '26-Set-2020',
+  title: 'Fim do registros de candidaturas',
+  text: '',
+},
+{
+  date: '27-Set-2020',
+  title: 'Início da propaganda eleitoral',
+  text: '',
+},
+{
+  date: '15-Nov-2020',
+  title: 'Primeiro turno das eleições',
+  text: '',
+},
+{
+  date: '29-Nov-2020',
+  title: 'Segundo turno das eleições',
+  text: '',
+},
 ]
