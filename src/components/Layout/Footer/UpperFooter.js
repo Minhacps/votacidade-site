2afd45a274b50750ded48227fc064420abf22f75
André Luís Bordignon <albordignon@gmail.com>
2020-07-22 17:02:01 -0300

    Incluido link para o site dos realizadores e dos apoiadores.
    Co-authored-by: João Aleixo  <joaovitordias92040@gmail.com>

--- src/components/Layout/Footer/UpperFooter.js
+++ src/components/Layout/Footer/UpperFooter.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'gatsby'
-import { Container, Row, Col } from 'reactstrap'
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import { Link as LinkLogo } from "../../Link"
+import { Container, Row, Col } from "reactstrap"
 
-import { Title } from '../../Title'
-import { Button } from '../../Button'
+import { Title } from "../../Title"
+import { Button } from "../../Button"
 
-import { getColor } from '../../../utils/styles'
+import { getColor } from "../../../utils/styles"
 
 const StyledFooter = styled.div`
   background-color: ${getColor};
@@ -23,7 +24,7 @@ const ListaDeContato = styled.ul`
   color: #fff;
 
   strong::after {
-    content: '|';
+    content: "|";
     display: inline-block;
     margin-left: 5px;
   }
@@ -37,26 +38,46 @@ const Contato = ({ cidade, contato }, index) => (
 
 const Partnership = (partner, index) => (
   <Col key={`partner-${index}`} xs="4" sm="3" md="2" className="mb-3">
-    <img src={partner.logo} alt={`Logotipo ${partner.nome}`} title={partner.nome} className="img-fluid d-block mx-auto" />
+    <LinkLogo tag="a" href={partner.link}>
+      <img
+        src={partner.logo}
+        alt={`Logotipo ${partner.nome}`}
+        title={partner.nome}
+        className="img-fluid d-block mx-auto"
+      />
+    </LinkLogo>
   </Col>
 )
 
-export const UpperFooter = ({ contatos = [], makers = [], supporters = [] }) => (
+export const UpperFooter = ({
+  contatos = [],
+  makers = [],
+  supporters = [],
+}) => (
   <StyledFooter className="py-3">
     <Container>
       <Row className="mt-3">
         <Col xs="12" lg="6">
-          <Title tag="h4" size="1.1em" color="secondaryColor">Contato</Title>
-          <ListaDeContato>
-            {contatos.map(Contato)}
-          </ListaDeContato>
+          <Title tag="h4" size="1.1em" color="secondaryColor">
+            Contato
+          </Title>
+          <ListaDeContato>{contatos.map(Contato)}</ListaDeContato>
         </Col>
 
         <Col xs="12" lg="6">
-          <Title tag="h4" size="1.1em" color="secondaryColor">Apoie o projeto!</Title>
-          <p style={{ color: '#fff' }}>O Vota Cidade foi idealizado por cinco cidades da Rede Nossas Cidades, organizações sem fins lucrativos, que tem por objetivo fortalecer e incentivar a cultura da participação em questões públicas.</p>
+          <Title tag="h4" size="1.1em" color="secondaryColor">
+            Apoie o projeto!
+          </Title>
+          <p style={{ color: "#fff" }}>
+            O Vota Cidade foi idealizado por cinco cidades da Rede Nossas
+            Cidades, organizações sem fins lucrativos, que tem por objetivo
+            fortalecer e incentivar a cultura da participação em questões
+            públicas.
+          </p>
           <Link to="/o-projeto">
-            <StyledButton bold light size="lg" color="secondaryColor">Saiba Mais</StyledButton>
+            <StyledButton bold light size="lg" color="secondaryColor">
+              Saiba Mais
+            </StyledButton>
           </Link>
         </Col>
       </Row>
@@ -65,19 +86,36 @@ export const UpperFooter = ({ contatos = [], makers = [], supporters = [] }) => 
 
       <Row>
         <Col xs="12" lg="10">
-          <Title tag="h4" size="1.1em" color="secondaryColor" className="mb-3">Realização</Title>
-          <Row className="align-items-center">
-            {makers.map(Partnership)}
-          </Row>
+          <Title tag="h4" size="1.1em" color="secondaryColor" className="mb-3">
+            Realização
+          </Title>
+          <Row className="align-items-center">{makers.map(Partnership)}</Row>
         </Col>
         <Col xs="12" lg="2">
-          <Title tag="h4" size="1.1em" color="secondaryColor" className="mb-3">Apoio</Title>
+          <Title tag="h4" size="1.1em" color="secondaryColor" className="mb-3">
+            Apoio
+          </Title>
           <Row className="align-items-center">
             {supporters.map(supporter => {
               return (
-                <Col key={supporter} xs="4" sm="3" md="2" lg="12" className="mb-3">
-                  <img src={supporter.logo} alt={`Logotipo ${supporter.nome}`} title={supporter.nome} className="img-fluid d-block mx-auto" />
-                </Col>)
+                <Col
+                  key={supporter}
+                  xs="4"
+                  sm="3"
+                  md="2"
+                  lg="12"
+                  className="mb-3"
+                >
+                  <LinkLogo tag="a" href={supporter.link}>
+                    <img
+                      src={supporter.logo}
+                      alt={`Logotipo ${supporter.nome}`}
+                      title={supporter.nome}
+                      className="img-fluid d-block mx-auto"
+                    />
+                  </LinkLogo>
+                </Col>
+              )
             })}
           </Row>
         </Col>
--- src/data/makers.js
+++ src/data/makers.js
@@ -2,29 +2,36 @@ export const makers = [
   {
     nome: 'Campinas Front-end',
     logo: require('../images/logo-front-campinas.svg'),
+    link: 'https://campinasfrontend.com.br/'
   },
   {
     nome: 'IFSP Campinas',
     logo: require('../images/logo-ifsp-campinas.png'),
+    link: 'https://cmp.ifsp.edu.br/'
   },
   {
     nome: 'Minha Campinas',
     logo: require('../images/logo-minha-campinas.png'),
+    link: 'https://www.minhacampinas.org.br/'
   },
   {
     nome: 'Minha Jampa',
     logo: require('../images/logo-minha-jampa.png'),
+    link: 'https://minhajampa.org.br'
   },
   {
     nome: 'Minha Porto Alegre',
     logo: require('../images/logo-minha-poa.png'),
+    link: 'https://minhaportoalegre.org.br'
   },
   {
     nome: 'Meu Recife',
     logo: require('../images/logo-meu-recife.png'),
+    link: 'https://www.meurecife.org.br/'
   },
   {
     nome: 'Minha Campina',
     logo: require('../images/logo-minha-campina.png'),
+    link: 'https://www.facebook.com/MinhaCampinaRNC/'
   },
 ]
