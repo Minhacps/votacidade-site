ab5ba44573778971b8d28781f215b5b9bab4b214
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-07-25 10:52:20 -0300

    Atualiza formato de data do calendário

--- src/data/calendario.js
+++ src/data/calendario.js
@@ -1,31 +1,31 @@
 export const datasDoCalendario = [
   {
-  date: '31-Ago-2020',
+  date: '31 de Agosto de 2020',
   title: 'Início da convenções partidárias',
   text: '',
 },
 {
-  date: '16-Set-2020',
+  date: '16 de Setembro de 2020',
   title: 'Fim convenções partidárias',
   text: '',
 },
 {
-  date: '26-Set-2020',
+  date: '26 de Setembro de 2020',
   title: 'Fim do registros de candidaturas',
   text: '',
 },
 {
-  date: '27-Set-2020',
+  date: '27 de Setembro de 2020',
   title: 'Início da propaganda eleitoral',
   text: '',
 },
 {
-  date: '15-Nov-2020',
+  date: '15 de Novembro de 2020',
   title: 'Primeiro turno das eleições',
   text: '',
 },
 {
-  date: '29-Nov-2020',
+  date: '29 de Novembro de 2020',
   title: 'Segundo turno das eleições',
   text: '',
 },
