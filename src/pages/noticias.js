320518aa4fb18df7337817b0524c807165690102
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-07-25 16:40:53 -0300

    Adiciona autor da noticia

--- src/pages/noticias.js
+++ src/pages/noticias.js
@@ -28,6 +28,12 @@ const StyledLink = styled.a`
   }
 `
 
+const TagAutor = styled.p`
+  margin-bottom: 10px;
+  color: #4a4a4a;
+  font-size: 13px;
+`
+
 const PageNews = () => {
   return (
     <Layout>
@@ -52,7 +58,8 @@ const PageNews = () => {
               <Col md="6" className="mb-4 d-flex align-items-stretch">
                 <Card>
                   <CardBody className="d-flex flex-column">
-                    <Title className="h4">Novas regras para as eleições 2020</Title>
+                    <Title className="h4" style={{ marginBottom: 0 }}>Novas regras para as eleições 2020</Title>
+                    <TagAutor>Material por: Politize</TagAutor>
                     <CardText>Em 2020, ocorrem as eleições municipais. Entretanto, muitas reformas foram feitas nessa questão.</CardText>
                     <StyledLink rel="noopener noreferrer" href="https://www.politize.com.br/novas-regras-para-as-eleicoes-2020/" target="_blank">Leia mais</StyledLink>
                   </CardBody>
@@ -61,7 +68,8 @@ const PageNews = () => {
               <Col md="6" className="mb-4 d-flex align-items-stretch">
                 <Card>
                   <CardBody className="d-flex flex-column">
-                    <Title className="h4">Fundo Eleitoral</Title>
+                    <Title className="h4" style={{ marginBottom: 0 }}>Fundo Eleitoral</Title>
+                    <TagAutor>Material por: Politize</TagAutor>
                     <CardText>O fundo eleitoral não é o fundo partidário. Entenda mais o que significa esse novo fundo.</CardText>
                     <StyledLink rel="noopener noreferrer" href="https://www.politize.com.br/fundo-eleitoral/" target="_blank">Leia mais</StyledLink>
                   </CardBody>
@@ -70,7 +78,8 @@ const PageNews = () => {
               <Col md="6" className="mb-4 d-flex align-items-stretch">
                 <Card>
                   <CardBody className="d-flex flex-column">
-                    <Title className="h4">Cotas de gênero em eleições</Title>
+                    <Title className="h4" style={{ marginBottom: 0 }}>Cotas de gênero em eleições</Title>
+                    <TagAutor>Material por: Politize</TagAutor>
                     <CardText>Entre idas e vindas, desde os anos 90, o Brasil adota mecanismos de compensação para corrigir a subrepresentatividade feminina.</CardText>
                     <StyledLink rel="noopener noreferrer" href="https://www.politize.com.br/cotas-de-genero-em-eleicoes/" target="_blank">Leia mais</StyledLink>
                   </CardBody>
@@ -79,7 +88,8 @@ const PageNews = () => {
               <Col md="6" className="mb-4 d-flex align-items-stretch">
                 <Card>
                   <CardBody className="d-flex flex-column">
-                    <Title className="h4">5 séries sobre política</Title>
+                    <Title className="h4" style={{ marginBottom: 0 }}>5 séries sobre política</Title>
+                    <TagAutor>Material por: Politize</TagAutor>
                     <CardText>Que tal entender mais de política assistindo séries? Veja algumas séries citadas por esse artigo e a polêmica que elas geram.  </CardText>
                     <StyledLink rel="noopener noreferrer" href="https://www.politize.com.br/series-sobre-politica-2/" target="_blank">Leia mais</StyledLink>
                   </CardBody>
