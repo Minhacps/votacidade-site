00d8f6a69c79d1462540ed35991bdbd7b9a076f1
André Luís Bordignon <albordignon@gmail.com>
2020-07-27 12:39:22 -0300

    Inicio da inclusão de novos itens no menu.

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -21,18 +21,39 @@ const StyledLink = styled(NavLink)`
   text-transform: uppercase;
   font-weight: bold;
   font-size: 1rem;
-  color: ${({ theme }) =>  theme.darkGray} !important;
+  color: ${({ theme }) => theme.darkGray} !important;
 
   .nav-item:not(:last-child) & {
     margin-right: 15px;
   }
 
   &.active {
-    color: ${({ theme }) =>  theme.primaryColor} !important;
+    color: ${({ theme }) => theme.primaryColor} !important;
   }
   
   &:hover {
-    color: ${({ theme }) =>  theme.darkGray} !important;
+    color: ${({ theme }) => theme.darkGray} !important;
+  }
+`
+
+const ExternalLink = styled.a`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 1rem;
+  
+  vertical-align:middle;
+  color: ${({ theme }) => theme.darkGray} !important;
+
+  .nav-item:not(:last-child) & {
+    margin-right: 15px;
+  }
+
+  &.active {
+    color: ${({ theme }) => theme.primaryColor} !important;
+  }
+  
+  &:hover {
+    color: ${({ theme }) => theme.darkGray} !important;
   }
 `
 
@@ -42,13 +63,22 @@ export const Menu = () => {
 
   return (
     <Nav className="mr-0 ml-auto" navbar>
-      {menuLinks && menuLinks.map(link => (
-        <NavItem key={link.name}>
-          <StyledLink tag={Link} to={link.link} activeClassName="active">
-            {link.name}
-          </StyledLink>
-        </NavItem>
-      ))}
+      {menuLinks && menuLinks.map(link => {
+        return (
+          <NavItem key={link.name}>
+            <StyledLink tag={Link} to={link.link} activeClassName="active">
+              {link.name}
+            </StyledLink>
+          </NavItem>
+        )
+      })}
+      <NavItem>
+        <ExternalLink
+          href="https://drive.google.com/drive/folders/1xQh5dm-XkmQL_deO9sRueERMYpHoCj2a?usp=sharing"
+          activeClassName="active"
+        >Midia Kit
+        </ExternalLink>
+      </NavItem>
     </Nav>
   )
 }
