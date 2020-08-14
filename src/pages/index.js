070a9c2435476fbaa0ec849c5b0a848fa3be5903
André Luís Bordignon <albordignon@gmail.com>
2020-08-14 10:47:15 -0300

    Implementanda a página inicial com apontamento para o app.
    Co-authored-by: João Aleixo  <joaovitordias92040@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,25 @@
 import React from "react"
-import styled from 'styled-components'
+import styled from "styled-components"
 import {
-  Container, Row, Col,
-  Card, CardImg, CardBody, CardTitle, CardText
-} from 'reactstrap'
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+} from "reactstrap"
 
-import GroupTitle from '../components/GroupTitle'
-import { Title } from '../components/Title'
+import GroupTitle from "../components/GroupTitle"
+import { Title } from "../components/Title"
 import { Layout } from "../components/Layout"
-import { CTAEmail } from "../components/CTAEmail"
+// import { CTAEmail } from "../components/CTAEmail"
+import { alfabeticOrder } from '../utils/helper';
+import CidadesApp from "../components/CidadesApp";
 
 import SEO from "../components/seo"
+import { cidades } from '../data';
 
 const StyledCard = styled(Card)`
   border-radius: 10px;
@@ -18,21 +27,48 @@ const StyledCard = styled(Card)`
 `
 
 const StyledHeader = styled.header`
-  background-color: ${({theme}) => theme.lightSecondary};
-  background-image: url(${require('../images/pattern-alto.png')});
+  background-color: ${({ theme }) => theme.lightSecondary};
+  background-image: url(${require("../images/pattern-alto.png")});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
 `
 
+const CidadesWrapper = styled.section`
+  background-color: ${({ theme }) => theme.lightSecondary};
+  margin-bottom: 40px;
+
+  .cidades-wrapper {
+    display: flex;
+  }
+
+  @media (min-width: 800px) {
+    margin-top: -20px;
+  }
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Inicial" />
-    <StyledHeader className="pt-5 pb-5">
-      <CTAEmail
-        title='Inscreva-se e saiba em primeira mão do lançamento!'
-        subtitle="Nosso projeto está crescendo e esse ano estaremos em 5 cidades!"
-      />
+    <StyledHeader>
+      <CidadesWrapper>
+        <Container>
+          <GroupTitle
+            subTitle="Selecione o munícipio onde você vai votar."
+            className="mt-3 mb-5"
+          />
+          <Row
+            className="cidades-wrapper mt-3"
+            xs="2"
+            sm="3"
+            md="4"
+            lg="5"
+            noGutters
+          >
+            {cidades.sort(alfabeticOrder("title")).map(CidadesApp)}
+          </Row>
+        </Container>
+      </CidadesWrapper>
     </StyledHeader>
     <Container className="py-5">
       <GroupTitle
@@ -41,39 +77,97 @@ const IndexPage = () => (
         className="mt-3 mb-5"
       />
       <Row noGutters>
-        <Col md="6" xl="3" className="pr-md-2 pr-lg-3 pr-xl-2 mb-4 d-flex align-items-stretch">
+        <Col
+          md="6"
+          xl="3"
+          className="pr-md-2 pr-lg-3 pr-xl-2 mb-4 d-flex align-items-stretch"
+        >
           <StyledCard>
-            <CardImg top width="100%" src={require('../images/como-funciona.png')} alt="Como funciona" />
+            <CardImg
+              top
+              width="100%"
+              src={require("../images/como-funciona.png")}
+              alt="Como funciona"
+            />
             <CardBody>
-              <CardTitle><Title size="24px">Como funciona?</Title></CardTitle>
-              <CardText>Você responderá um questionário de 30 perguntas, com questões variadas sobre políticas públicas como transporte, saneamento básico, drogas, etc.</CardText>
+              <CardTitle>
+                <Title size="24px">Como funciona?</Title>
+              </CardTitle>
+              <CardText>
+                Você responderá um questionário de 30 perguntas, com questões
+                variadas sobre políticas públicas como transporte, saneamento
+                básico, drogas, etc.
+              </CardText>
             </CardBody>
           </StyledCard>
         </Col>
-        <Col md="6" xl="3" className="pl-md-2 pl-lg-3 px-xl-2 mb-4 d-flex align-items-stretch">
+        <Col
+          md="6"
+          xl="3"
+          className="pl-md-2 pl-lg-3 px-xl-2 mb-4 d-flex align-items-stretch"
+        >
           <StyledCard>
-            <CardImg top width="100%" src={require('../images/afinidade.png')} alt="Afinidade" />
+            <CardImg
+              top
+              width="100%"
+              src={require("../images/afinidade.png")}
+              alt="Afinidade"
+            />
             <CardBody>
-              <CardTitle><Title size="24px">Afinidade</Title></CardTitle>
-              <CardText>Nós cruzaremos as suas respostas com os candidatos e candidatas cadastradas e te mostraremos quem pensa parecido com você!</CardText>
+              <CardTitle>
+                <Title size="24px">Afinidade</Title>
+              </CardTitle>
+              <CardText>
+                Nós cruzaremos as suas respostas com os candidatos e candidatas
+                cadastradas e te mostraremos quem pensa parecido com você!
+              </CardText>
             </CardBody>
           </StyledCard>
         </Col>
-        <Col md="6" xl="3" className="pr-md-2 pr-lg-3 px-xl-2 mb-4 d-flex align-items-stretch">
+        <Col
+          md="6"
+          xl="3"
+          className="pr-md-2 pr-lg-3 px-xl-2 mb-4 d-flex align-items-stretch"
+        >
           <StyledCard>
-            <CardImg top width="100%" src={require('../images/quero-mais.png')} alt="Quero mais" />
+            <CardImg
+              top
+              width="100%"
+              src={require("../images/quero-mais.png")}
+              alt="Quero mais"
+            />
             <CardBody>
-              <CardTitle><Title size="24px">Quero mais!</Title></CardTitle>
-              <CardText>Você poderá pesquisar e acessar o perfil de qualquer candidato ou candidata cadastrada para ver as respostas, comparar e tomar a sua decisão.</CardText>
+              <CardTitle>
+                <Title size="24px">Quero mais!</Title>
+              </CardTitle>
+              <CardText>
+                Você poderá pesquisar e acessar o perfil de qualquer candidato
+                ou candidata cadastrada para ver as respostas, comparar e tomar
+                a sua decisão.
+              </CardText>
             </CardBody>
           </StyledCard>
         </Col>
-        <Col md="6" xl="3" className="pl-md-2 pl-lg-3 pl-xl-2 mb-4 d-flex align-items-stretch">
+        <Col
+          md="6"
+          xl="3"
+          className="pl-md-2 pl-lg-3 pl-xl-2 mb-4 d-flex align-items-stretch"
+        >
           <StyledCard>
-            <CardImg top width="100%" src={require('../images/seguranca-dos-dados.png')} alt="Segurança dos Dados" />
+            <CardImg
+              top
+              width="100%"
+              src={require("../images/seguranca-dos-dados.png")}
+              alt="Segurança dos Dados"
+            />
             <CardBody>
-              <CardTitle><Title size="24px">E os dados?</Title></CardTitle>
-              <CardText>Nós nos preocupamos e respeitamos muito seus dados e por isso daremos a opção de responder o questionário de forma anônima.</CardText>
+              <CardTitle>
+                <Title size="24px">E os dados?</Title>
+              </CardTitle>
+              <CardText>
+                Nós nos preocupamos e respeitamos muito seus dados e por isso
+                daremos a opção de responder o questionário de forma anônima.
+              </CardText>
             </CardBody>
           </StyledCard>
         </Col>
