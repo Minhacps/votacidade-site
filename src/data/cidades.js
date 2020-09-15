4bed19eba3cb7f7f771462193562a9c095649f16
André Luís Bordignon <albordignon@gmail.com>
2020-09-15 07:55:20 -0300

    Testes com o fluxo de aplicação

--- src/data/cidades.js
+++ src/data/cidades.js
@@ -3,30 +3,30 @@ export const cidades = [
     title: 'Campinas',
     image: require('../images/photos_city/campinas-fundo.jpg'),
     linkTo: 'https://www.eufaco.minhacampinas.org.br',
-    appLink: 'http://app.vota.org.br/campinas'
+    appLink: 'http://app.vota.org.br/'
   },
   {
     title: 'João Pessoa',
     image: require('../images/photos_city/jampa-fundo.png'),
     linkTo: 'https://www.minhajampa.org.br/#block-14338',
-    appLink: 'https://app.vota.org.br/joao-pessoa'
+    appLink: 'https://app.vota.org.br/'
   },
   {
     title: 'Porto Alegre',
     image: require('../images/photos_city/poa-fundo.jpg'),
     linkTo: 'https://www.minhaportoalegre.org.br/#block-12312',
-    appLink: 'https://app.vota.org.br/porto-alegre'
+    appLink: 'https://app.vota.org.br/'
   },
   {
     title: 'Recife',
     image: require('../images/photos_city/recife-fundo.jpg'),
     linkTo: 'https://www.meurecife.org.br/#block-4456',
-    appLink: 'https://app.vota.org.br/recife'
+    appLink: 'https://app.vota.org.br/'
   },
   {
     title: 'Campina Grande',
     image: require('../images/photos_city/campina-grande-fundo.jpg'),
     linkTo: 'https://www.facebook.com/MinhaCampinaRNC/',
-    appLink: 'https://app.vota.org.br/campina-grande'
+    appLink: 'https://app.vota.org.br/'
   },
 ]
