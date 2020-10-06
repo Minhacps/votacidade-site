aa9057e86fef40c4303fa201e33ed9550f094793
João <joaovitordias92040@gmail.com>
2020-10-05 22:34:06 -0300

    Criado componente PurpleBlock

---
+++ src/components/PurpleBlock.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Title } from '../components/Title';
+import { Button } from '../components/Button';
+import styled from 'styled-components';
+
+const PurpleContainer = styled(Container)`
+    background-color: #662D91;
+    text-align: center;
+    padding: 48px;
+`;
+
+const PurpleBlock = () => {
+    return (
+        <PurpleContainer fluid>
+            <Container>
+                <Row>
+                    <Col>
+                        <Title tag="h3" color="white " className="h4">É candidato(a) a vereador(a)? Garanta sua participação</Title>
+                    </Col>
+                    <Col lg={3}>
+                        <Button block bold size="lg" color="primaryColor" style={{border: '2px solid white'}} className="mt-5-md">COMEÇAR</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </PurpleContainer>
+    )
+
+}
+
+export default PurpleBlock;
